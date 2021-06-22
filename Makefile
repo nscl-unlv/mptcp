208b748f2396004ade6a1544cbd0e507cc2366ef
phillipe-austria <austrp1@unlv.nevada.edu>
2021-06-21 19:33:39 -0700

    add reset command

--- Makefile
+++ Makefile
@@ -2,6 +2,10 @@
 run_test:
 	./scripts/run-mptcp-test.sh
 
+.PHONY: reset
+reset:
+	./scripts/reset-network-settings.sh
+
 .PHONY: clean
 clean:
 	rm -rf ./results/*.*
--- scripts/reset-network-settings.sh
+++ scripts/reset-network-settings.sh
@@ -2,7 +2,7 @@
 
 # resets default congestion algorithm and mptcp settings
 
-echo "reseting congestion control and mptcp settings..."
+echo "Reseting congestion control and mptcp settings..."
 sudo sysctl -w net.ipv4.tcp_congestion_control=cubic
 sudo sysctl -w net.mptcp.mptcp_scheduler=default
 sudo sysctl -w net.mptcp.mptcp_path_manager=fullmesh
