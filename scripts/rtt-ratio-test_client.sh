bf5befc49ef2948ad4a2f6857efda42cbd86b64a
paustria <austrp1@unlv.nevada.edu>
2021-09-11 12:03:35 -0700

    add loop to run test

--- scripts/rtt-ratio-test_client.sh
+++ scripts/rtt-ratio-test_client.sh
@@ -4,8 +4,8 @@
 
 INTF_1="eno1"
 INTF_2="enp7s0"
-INTF_3="lo" # test
-IP_1="127.0.0.1"
+INTF_LO="lo" # test
+IP_LO="127.0.0.1"
 
 # constants
 PACKET_LOSS=0.1
@@ -48,9 +48,26 @@ FILE_SIZES=("128K" "1M" "10M")
 len_rtts=${#INTF2_RTTS[@]}
 len_sizes=${#FILE_SIZES[@]}
 
+# TODO: change to INTF_2
 for (( rtt=1; rtt<$len_rtts; rtt++ )); do
+    echo -e "test run: ${INTF2_RTTS[rtt]}ms delay, ${FILE_SIZES[size]}\n"
+
     for (( size=1; size<len_sizes; size++ )); do
-        echo "${INTF2_RTTS[rtt]}" ${FILE_SIZES[size]}
+        echo -e "adding traffic control rules to $INTF_1...\n"
+        sudo tc qdisc add dev eno1 root netem delay ${INTF2_RTTS[$rtt]}ms loss $PACKET_LOSS%
+        tc qdisc show dev $INTF_1
+
+        # TODO: change to INTF_1
+        echo -e "\ncapturing traffing on $INTF_1...\n"
+        sudo tcpdump -i $INTF_LO -w "$INTF_LO-$INTF1_RTT-${FILE_SIZES[0]}.pcap" &
+
+        # TODO: listen to INTF_2
+
+        echo -e "starting iperf: connecting to $IP_1, sending size ${FILE_SIZES[0]}...\n"
+        iperf3 -c $IP_LO -B $IP_LO -f m -n ${FILE_SIZES[0]} -b 1000M --logfile "iperf-${RATIOS[0]}-${FILE_SIZES[0]}.txt"
+
+        echo -e "stopping tcpdump...\n"
+        sudo pkill tcpdump
     done
 done
 
