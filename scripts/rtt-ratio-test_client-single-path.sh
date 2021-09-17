cc7fb46c41fce4fe9fed1c43cf38d2ae5afe0177
phillipe-austria <austrp1@unlv.nevada.edu>
2021-09-17 13:45:01 -0700

    remove tcpdump

--- scripts/rtt-ratio-test_client-single-path.sh
+++ scripts/rtt-ratio-test_client-single-path.sh
@@ -43,27 +43,11 @@ for (( run=1; run<=$RUNS_PER_TEST; run++ )); do
     for (( size=0; size<len_sizes; size++ )); do
 	    echo -e "\n\nSTARTING RUN $run: ${INTF2_RTTS[0]} delay on $INTF_1, ${FILE_SIZES[$size]}\n\n"
 
-	    # TEST lo
-	    #echo "capturing traffing on $INTF_LO..."
-	    #sudo tcpdump -i $INTF_LO -w "$INTF_LO-run_$run-$INTF1_RTT-${FILE_SIZES[0]}.pcap" &
-
-	    echo "capturing traffing on $INTF_1..."
-	    sudo tcpdump -i $INTF_1 -w "$INTF_1-run_$run-$INTF1_RTT-${INTF2_RTTS[0]}-${FILE_SIZES[$size]}.pcap" &
-        
-
-	    # TEST lo
-	    #echo "starting iperf: connecting to $IP_LO, sending size ${FILE_SIZES[0]}..."
-	    #iperf3 -c $IP_LO -B $IP_LO -f m -n ${FILE_SIZES[0]} -b 1000M --logfile "iperf-run_$run-${INTF1_RTT}-${INTF2_RTTS[0]}-${FILE_SIZES[0]}.txt"
-
 	    echo "starting iperf: connecting to $IP_SERVER, sending size ${FILE_SIZES[$size]}..."
 	    iperf3 -c $IP_SERVER -B $PRIMARY_PATH -f m -n ${FILE_SIZES[$size]} -b 1000M --logfile "iperf-run_$run-$INTF1_RTT-${INTF2_RTTS[0]}-${FILE_SIZES[$size]}.txt"
 
 	    echo "pausing to ensure capture..."
 	    sleep 3
-
-	    # stop capture
-	    echo "stopping tcpdump..."
-	    sudo pkill tcpdump
     done
 done
 
@@ -75,11 +59,6 @@ len_rtts=${#INTF2_RTTS[@]}
 for (( rtt=1; rtt<$len_rtts; rtt++ )); do
     for (( size=0; size<len_sizes; size++ )); do
 
-        # TEST
-        #echo "change traffic control rules on $INTF_LO..."
-        #sudo tc qdisc change dev $INTF_LO root netem delay ${INTF2_RTTS[$rtt]} loss $PACKET_LOSS%
-        #tc qdisc show dev $INTF_LO
-
         echo "change traffic control rules on $INTF_1..."
         sudo tc qdisc change dev $INTF_1 root netem delay ${INTF2_RTTS[$rtt]} loss $PACKET_LOSS%
         tc qdisc show dev $INTF_1
@@ -87,35 +66,16 @@ for (( rtt=1; rtt<$len_rtts; rtt++ )); do
         for (( run=1; run<=$RUNS_PER_TEST; run++ )); do
             echo -e "\n\nSTARTING RUN $run: ${INTF2_RTTS[$rtt]} delay on $INTF_1, ${FILE_SIZES[$size]}\n\n"
 
-            # TEST lo
-            #echo "capturing traffing on $INTF_LO..."
-            #sudo tcpdump -i $INTF_LO -w "$INTF_LO-run_$run-$INTF1_RTT-${FILE_SIZES[$size]}.pcap" &
-
-            echo "capturing traffing on $INTF_1..."
-            sudo tcpdump -i $INTF_1 -w "$INTF_1-run_$run-$INTF1_RTT-${INTF2_RTTS[$rtt]}-${FILE_SIZES[$size]}.pcap" &
-
-            # TEST lo
-            #echo "starting iperf: connecting to $IP_LO, sending size ${FILE_SIZES[$size]}..."
-            #iperf3 -c $IP_LO -B $IP_LO -f m -n ${FILE_SIZES[$size]} -b 1000M --logfile "iperf-run_$run-$INTF1_RTT-${INTF2_RTTS[$rtt]}-${FILE_SIZES[$size]}.txt"
-
             echo "starting iperf: connecting to $IP_SERVER, sending size ${FILE_SIZES[$size]}..."
             iperf3 -c $IP_SERVER -B $PRIMARY_PATH -f m -n ${FILE_SIZES[$size]} -b 1000M --logfile "iperf-run_$run-$INTF1_RTT-${INTF2_RTTS[$rtt]}-${FILE_SIZES[$size]}.txt"
 
             echo "pausing to ensure capture..."
             sleep 3
-
-            echo "stopping tcpdump..."
-            sudo pkill tcpdump
         done
     done
 done
 
 ######################### end #################################
-
-# TEST lo
-#echo -e "\nremoving traffic control rules\n"
-#sudo tc qdisc del dev $INTF_LO root netem
-#tc qdisc show dev $INTF_LO
 
 echo -e "\nremoving traffic control rules from $INTF_1"
 sudo tc qdisc del dev $INTF_1 root netem
