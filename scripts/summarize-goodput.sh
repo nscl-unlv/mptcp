7127b6aa165c597acb44ce2eb76b8c234cd1733f
Phillipe Austria <austrp1@unlv.nevada.edu>
2021-10-04 17:14:42 -0700

    add script to summarize goodput, not finished

---
+++ scripts/summarize-goodput.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+CUR_DIR=$(pwd)
+TOTAL_RUNS=10
+
+# loop through directories
+for cca in */ ; do
+    #echo "$cca"
+
+    # loop through rtt-size folder
+    for rtt_size in $cca*; do
+        #echo "$rtt_size"
+
+        # loop through trial runs
+        bsum=0
+        for run in $rtt_size/*; do 
+            # echo "$run"
+
+            bitrate=$(cat $run | grep "receiver" | awk -F 'Bytes' '{print $2}' | sed 's/^ *//g' | cut -d ' ' -f 1)
+            bsum=$(echo "$bsum + $bitrate" | bc)
+        done # end run
+
+        #echo "bsum: $bsum"
+        bavg=$(echo "$bsum/$TOTAL_RUNS" | bc -l)
+        printf "%.2f\n" $bavg
+    done # end rtt_size
+done # end cca
